f5616ff3bb55aafdeeee91a9090a389e3d5161a5
YinggggFeng <yingfeng@andrew.cmu.edu>
2022-10-07 21:37:18 -0400

    un-commented code in the graph implementation

--- src/graph/graph.cpp
+++ src/graph/graph.cpp
@@ -19,106 +19,106 @@ Graph new_graph(int n){
     return graph;
 }
 
-// void set_type1_edge(Graph graph, int n1, int n2){
-//     int graph_size = get<3>(graph);
-//     if(n1 < 0 || n1 > graph_size){
-//         throw invalid_argument("set_type1_edge invalid n1");
-//         return;
-//     }
+void set_type1_edge(Graph graph, int n1, int n2){
+    int graph_size = get<3>(graph);
+    if(n1 < 0 || n1 > graph_size){
+        throw invalid_argument("set_type1_edge invalid n1");
+        return;
+    }
 
-//     if(n2 < 0 || n2 > graph_size){
-//         throw invalid_argument("set_type1_edge invalid n2");
-//         return;
-//     }
+    if(n2 < 0 || n2 > graph_size){
+        throw invalid_argument("set_type1_edge invalid n2");
+        return;
+    }
 
-//     subGraph type1G = get<0>(graph);
+    subGraph type1G = get<0>(graph);
     
-// }
+}
 
-// void rem_edge(Graph graph, int v1, int v2){
-//     edgeType** matrix = get<0>(graph);
-//     int n = get<1>(graph);
-//     int m = get<2>(graph);
+void rem_edge(Graph graph, int v1, int v2){
+    edgeType** matrix = get<0>(graph);
+    int n = get<1>(graph);
+    int m = get<2>(graph);
 
-//     if (v1 <= 0 || v1 > n){
-//         return;
-//     }
+    if (v1 <= 0 || v1 > n){
+        return;
+    }
 
-//     if (v2 <= 0 || v2 > m){
-//         return;
-//     }
+    if (v2 <= 0 || v2 > m){
+        return;
+    }
 
-//     matrix[v1][v2] = NULL_EDGE;
-//     return;
-// }
+    matrix[v1][v2] = NULL_EDGE;
+    return;
+}
 
-// edgeType get_edge(Graph graph, int v1, int v2){
-//     edgeType** matrix = get<0>(graph);
-//     int n = get<1>(graph);
-//     int m = get<2>(graph);
+edgeType get_edge(Graph graph, int v1, int v2){
+    edgeType** matrix = get<0>(graph);
+    int n = get<1>(graph);
+    int m = get<2>(graph);
 
-//     if (v1 <= 0 || v1 > n){
-//         throw std::invalid_argument( "invalid index" );
-//     }
+    if (v1 <= 0 || v1 > n){
+        throw std::invalid_argument( "invalid index" );
+    }
 
-//     if (v2 <= 0 || v2 > m){
-//         throw std::invalid_argument( "invalid index" );
-//     }
+    if (v2 <= 0 || v2 > m){
+        throw std::invalid_argument( "invalid index" );
+    }
 
-//     return matrix[v1][v2];
-// }
+    return matrix[v1][v2];
+}
 
-// vector<int> get_inNeighbors(Graph graph, int v) {
-//     edgeType** matrix = get<0>(graph);
-//     int n = get<1>(graph);
-//     vector<int> neighbors;
+vector<int> get_inNeighbors(Graph graph, int v) {
+    edgeType** matrix = get<0>(graph);
+    int n = get<1>(graph);
+    vector<int> neighbors;
 
-//     for (int v0 = 0; v0 < n; v0 ++) {
-//         if (matrix[v0][v] != NULL_EDGE) neighbors.push_back(v0);
-//     }
-//     return neighbors;
-// }
+    for (int v0 = 0; v0 < n; v0 ++) {
+        if (matrix[v0][v] != NULL_EDGE) neighbors.push_back(v0);
+    }
+    return neighbors;
+}
 
-// vector<int> get_outNeighbors(Graph graph, int v) {
-//     edgeType** matrix = get<0>(graph);
-//     int m = get<2>(graph);
-//     vector<int> neighbors;
+vector<int> get_outNeighbors(Graph graph, int v) {
+    edgeType** matrix = get<0>(graph);
+    int m = get<2>(graph);
+    vector<int> neighbors;
 
-//     for (int v0 = 0; v0 < m; v0 ++) {
-//         if (matrix[v][v0] != NULL_EDGE) neighbors.push_back(v0);
-//     }
-//     return neighbors;
-// }
+    for (int v0 = 0; v0 < m; v0 ++) {
+        if (matrix[v][v0] != NULL_EDGE) neighbors.push_back(v0);
+    }
+    return neighbors;
+}
 
-// vector<int> get_type2_inNeib(Graph graph, int v) {
-//     edgeType** matrix = get<0>(graph);
-//     int n = get<1>(graph);
-//     vector<int> neighbors;
+vector<int> get_type2_inNeib(Graph graph, int v) {
+    edgeType** matrix = get<0>(graph);
+    int n = get<1>(graph);
+    vector<int> neighbors;
 
-//     for (int v0 = 0; v0 < n; v0 ++) {
-//         if (matrix[v0][v] == TYPE2_EDGE) neighbors.push_back(v0);
-//     }
-//     return neighbors;
-// }
+    for (int v0 = 0; v0 < n; v0 ++) {
+        if (matrix[v0][v] == TYPE2_EDGE) neighbors.push_back(v0);
+    }
+    return neighbors;
+}
 
-// vector<int> get_type2_outNeib(Graph graph, int v) {
-//     edgeType** matrix = get<0>(graph);
-//     int m = get<2>(graph);
-//     vector<int> neighbors;
+vector<int> get_type2_outNeib(Graph graph, int v) {
+    edgeType** matrix = get<0>(graph);
+    int m = get<2>(graph);
+    vector<int> neighbors;
 
-//     for (int v0 = 0; v0 < m; v0 ++) {
-//         if (matrix[v][v0] == TYPE2_EDGE) neighbors.push_back(v0);
-//     }
-//     return neighbors;
-// }
+    for (int v0 = 0; v0 < m; v0 ++) {
+        if (matrix[v][v0] == TYPE2_EDGE) neighbors.push_back(v0);
+    }
+    return neighbors;
+}
 
-// void free_graph(Graph graph){
-//     edgeType** matrix = get<0>(graph);
-//     int n = get<1>(graph);
+void free_graph(Graph graph){
+    edgeType** matrix = get<0>(graph);
+    int n = get<1>(graph);
 
-//     for (int i = 0; i < n; i++){
-//         delete[] matrix[i];
-//     }
+    for (int i = 0; i < n; i++){
+        delete[] matrix[i];
+    }
 
-//     delete[] matrix;
-// }
+    delete[] matrix;
+}
