dfe2744c234a2eae9f5721ff7d6ad2190d376574
Ying Feng <yingfeng@linux-30.andrew.cmu.edu>
2022-10-21 00:05:53 -0400

    with merging message

--- src/ADG/ADG_utilities.h
+++ src/ADG/ADG_utilities.h
@@ -9,43 +9,41 @@
 
 using namespace std;
 
-int get_agentCnt(ADG adg);
+int get_agentCnt(ADG &adg);
 
-int get_stateCnt(ADG adg, int agent);
+int get_stateCnt(ADG &adg, int agent);
 
-int get_totalStateCnt(ADG adg);
+int get_totalStateCnt(ADG &adg);
 
-int compute_vertex(vector<int> accum_stateCnts, int agent, int state);
-
-int compute_vertex_ADG(ADG adg, int agent, int state);
+int compute_vertex(vector<int> &accum_stateCnts, int agent, int state);
 
 // Directed
-bool is_type2_edge(ADG adg, int agent1, int state1, int agent2, int state2);
+bool is_type2_edge(ADG &adg, int agent1, int state1, int agent2, int state2);
 
-void fix_type2_edge(ADG adg, int agent1, int state1, int agent2, int state2);
+void fix_type2_edge(ADG &adg, int agent1, int state1, int agent2, int state2);
 
-void fix_type2_edge_reversed(ADG adg, int agent1, int state1, int agent2, int state2);
+void fix_type2_edge_reversed(ADG &adg, int agent1, int state1, int agent2, int state2);
 
-void shift(ADG adg, int agent1, int state1, int agent2, int state2);
+void shift(ADG &adg, int agent1, int state1, int agent2, int state2);
 
 // Return an vector of agent-state pairs
-vector<pair<int, int>> get_switchable_inNeibPair(ADG adg, int agent, int state);
+vector<pair<int, int>> get_switchable_inNeibPair(ADG &adg, int agent, int state);
 
-vector<pair<int, int>> get_switchable_outNeibPair(ADG adg, int agent, int state);
+vector<pair<int, int>> get_switchable_outNeibPair(ADG &adg, int agent, int state);
 
-vector<pair<int, int>> get_nonSwitchable_inNeibPair(ADG adg, int agent, int state);
+vector<pair<int, int>> get_nonSwitchable_inNeibPair(ADG &adg, int agent, int state);
 
-vector<pair<int, int>> get_nonSwitchable_outNeibPair(ADG adg, int agent, int state);
+vector<pair<int, int>> get_nonSwitchable_outNeibPair(ADG &adg, int agent, int state);
 
-vector<pair<int, int>> get_inNeibPair(ADG adg, int agent, int state);
+vector<pair<int, int>> get_inNeibPair(ADG &adg, int agent, int state);
 
-vector<pair<int, int>> get_outNeibPair(ADG adg, int agent, int state);
+vector<pair<int, int>> get_outNeibPair(ADG &adg, int agent, int state);
 
-Location get_state_target(ADG adg, int agent, int state);
+Location get_state_target(ADG &adg, int agent, int state);
 
-ADG copy_ADG(ADG adg);
+ADG copy_ADG(ADG &adg);
 
 // Run DFS
-bool detectCycle(ADG adg, int agent, int state);
+bool detectCycle(ADG &adg, int agent, int state);
 
-void free_underlying_graph(ADG adg);
+void free_underlying_graph(ADG &adg);
