001117f4ec7ecf85ff67b184578ca07e2c8cf3d7
Rivers <rivers@foxmail.com>
2024-03-02 16:28:53 -0500

    add scripts to run multiple experiments

---
+++ script/get_stats.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import os
+import numpy as np
+
+result_folder="output/2024_03_02_15_29_04"
+main_result_folder=os.path.join(result_folder,"main")
+stats_ofp=os.path.join(result_folder,"stats.csv")
+time_limit=90
+
+exp_headers=["algo_name","map_name","agent_num","instance_idx","trial_idx"]
+result_headers=["search_time","total_time","ori_total_cost","ori_trunc_cost","total_cost","trunc_cost",
+         "explored_node","pruned_node","added_node","vertex","sw_edge","heuristic_time","branch_time",
+         "sort_time","priority_queue_time","copy_free_graph_time","termination_time","dfs_time"]
+headers=exp_headers+result_headers
+
+data=[]
+for file_name in os.listdir(main_result_folder):
+    file_path=os.path.join(main_result_folder,file_name)
+    name,ext=os.path.splitext(file_name)
+    algo_name=ext[1:]
+    idx_map=name.find("map_")
+    idx_ins=name.find("ins_")
+    idx_an=name.find("an_")
+    idx_tr=name.find("tr_")
+    map_name=name[idx_map+4:idx_ins-1]
+    instance_idx=int(name[idx_ins+4:idx_an-1])
+    agent_num=int(name[idx_an+3:idx_tr-1])
+    trial_idx=int(name[idx_tr+3:])
+    datum=[algo_name,map_name,agent_num,instance_idx,trial_idx]
+    with open(file_path) as f:
+        line=f.readline()
+        records=line.strip().split(",")
+        records=[int(v) if len(v)>0 else np.nan for v in records]
+        datum+=records
+    data.append(datum)
+    
+df=pd.DataFrame(data,columns=headers)
+
+df.to_csv(stats_ofp,index="index")
+
+for column in df.columns:
+    if column.find("time")!=-1:
+        df[column]=df[column]/(10**6)
+
+print(df.groupby(by=["algo_name","map_name","agent_num"]).mean())
+        
---
+++ script/run_exps.py
@@ -0,0 +1,135 @@
+import os
+import time
+import multiprocessing
+import pandas as pd
+import subprocess
+
+exe_path="./build/sadg"
+path_folder="example/path"
+file_names_fp="example/path_file_names.csv"
+
+timestamp=time.strftime("%Y_%m_%d_%H_%M_%S")
+# TODO(rivers): merge output into one or separate them somehow?
+output_folder="output/{}".format(timestamp)
+delay_prob=10
+delay_steps_low=10
+delay_steps_high=20
+time_limit=90
+
+instance_idxs=list(range(1,25+1)) # this is the number provided by the benchmark
+num_trials=6
+
+# setting: [agent_num_start, agent_num_end, agent_num_step, max_process_num]
+maps = {
+        "random-32-32-10":[25,50,5,32],
+        # "warehouse-10-20-10-2-1":[40,90,10,32],
+        # "Paris_1_256": [30,80,10,32],
+        # "lak303d": [15,35,4,32]
+       }
+
+main_output_folder=os.path.join(output_folder,"main")
+fail_output_folder=os.path.join(output_folder,"fail")
+aux_output_folder=os.path.join(output_folder,"aux")
+
+os.makedirs(main_output_folder,exist_ok=True)
+os.makedirs(fail_output_folder,exist_ok=True)
+os.makedirs(aux_output_folder,exist_ok=True)
+
+df=pd.read_csv(file_names_fp,index_col="index")
+df.sort_values(by=["map_name","agent_num","instance_idx"],inplace=True,ignore_index=True)
+
+# filter
+def keep(row):
+    map_name = row["map_name"]
+    agent_num = row["agent_num"]
+    instance_idx = row["instance_idx"]
+    if map_name not in maps:
+        return False
+    
+    setting = maps[map_name]
+    agent_num_start,agent_num_end,agent_num_step,max_process_num=setting
+    
+    if agent_num<agent_num_start or agent_num>=agent_num_end+agent_num_step:
+        return False
+    
+    if instance_idx not in instance_idxs:
+        return False
+    
+    return True
+
+df = df[df.apply(keep,axis=1)]
+# df = df.reset_index(drop=True)
+
+def log_fail(output_name,result=None,exception=None):
+    fail_output_file_path=os.path.join(fail_output_folder,output_name+".fail")
+    with open(fail_output_file_path,'w') as f:
+        if result is not None:
+            f.write("stdout: \n")
+            f.write(result.stdout)
+            f.write("\n")
+            f.write("stderr: \n")
+            f.write(result.stderr)
+        if exception is not None:
+            f.write("exception: \n")
+            f.write(exception)
+
+def run(cmd,output_name):
+    try:
+        result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+        
+        if result.returncode == 0:
+            print("[SUCC] {}".format(cmd))
+            print(result.stdout)
+        else:
+            print("[FAIL] {}".format(cmd))
+            print(result.stdout)
+            print(result.stderr)
+            log_fail(output_name,result=result)
+    except Exception:
+        import traceback
+        print("[EXCEPTION]", traceback.format_exc())
+        log_fail(output_name,exception=traceback.format_exc())
+    
+
+for map_name,setting in maps.items():
+    print("processing map {}".format(map_name))
+    
+    agent_num_start,agent_num_end,agent_num_step,max_process_num=setting
+    
+    pool=multiprocessing.Pool(max_process_num)
+    
+    exps=df[df["map_name"]==map_name].values.tolist()
+    
+    cmds=[]
+    output_names=[]
+    for exp in exps:
+        path_file_name,map_name,instance_idx,agent_num=exp
+        path_file_path=os.path.join(path_folder,path_file_name)
+        for trial_idx in range(num_trials):
+            trail_name="map_{}_ins_{}_an_{}_tr_{}".format(map_name,instance_idx,agent_num,trial_idx)
+            output_names.append(trail_name)
+            graph_stats_ofp=os.path.join(main_output_folder,trail_name+".graph")
+            simul_stats_ofp=os.path.join(main_output_folder,trail_name+".simul")
+            locations_ofp=os.path.join(aux_output_folder,trail_name+".locs")
+            execution_ofp=os.path.join(aux_output_folder,trail_name+".exec")
+            setup_ofp=os.path.join(aux_output_folder,trail_name+".setup")
+            
+            if os.path.exists(graph_stats_ofp) and os.path.exists(simul_stats_ofp):
+                print("{} exist. skip...".format(trail_name))
+                continue                
+    
+            print("run {}".format(trail_name))    
+
+            # we only require 1-robust, so --kDelay 1
+            cmd = f"{exe_path} -p {path_file_path} -d {delay_prob} -l {delay_steps_low} -h {delay_steps_high} -t {time_limit} " \
+                  f"-g {graph_stats_ofp} -s {simul_stats_ofp} " \
+                  f"-c {locations_ofp} -e {execution_ofp} -r {setup_ofp}"
+            
+            cmds.append(cmd)
+    
+    # for cmd,output_name in zip(cmds,output_names):
+    #     print(output_name,cmd)
+            
+    pool.starmap(run,zip(cmds,output_names))
+    
+
